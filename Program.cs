28cf7f8fb8eff527b12550d4e623903c62c572fc
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-09-30 01:54:06 -0700

    add pastry prompt and final checkout logic

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Bakery.Deals;
 
 namespace Bakery
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("---------------------------------------------------------");
             Console.WriteLine("Welcome to Pierre's Bakery! Our loaves of bread are $5 and our pastries are only $2!");
@@ -15,12 +15,18 @@ namespace Bakery
             Console.WriteLine("Buy any pastry for $2 or 3 for $5!");
             Console.WriteLine("---------------------------------------------------------");
             Console.WriteLine("Please enter the number of loaves of bread you'd like to order today:");            
+            Console.WriteLine("---------------------------------------------------------");
             int UserBreadInput = int.Parse(Console.ReadLine());
-            Console.WriteLine("---------------------------------------------------------");
+            Bread userBread = new Bread(UserBreadInput, 0);
             Console.WriteLine("Please enter the number of patries you'd like to order today:");
-            int UserPastryInput = int.Parse(Console.ReadLine());
             Console.WriteLine("---------------------------------------------------------");
-
+            int UserPastryInput = int.Parse(Console.ReadLine());
+            Pastry userPastry = new Pastry(UserPastryInput, 0);
+            userBread.BreadDeal(UserBreadInput);
+            userPastry.PastryDeal(UserPastryInput);
+            int Total = userPastry.PastryCost + userBread.BreadCost;
+            Console.WriteLine("Your total today will be $" + Total + "! Thank you for your order of " + userBread.BreadLoaves + " loaves of bread and " + userPastry.PastryNumber + " pastries today, we hope to see you again soon!");
+            Console.WriteLine("---------------------------------------------------------");
         }
     }
 }

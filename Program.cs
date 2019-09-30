7acd8561b15b9d555231d971a77c1d5dc3ce1ac0
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-09-30 00:16:58 -0700

    add welcome message to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace bakery
+namespace Bakery
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Welcome to Pierre's Bakery! Our loaves of bread are $5 and our pastries are only $2!");
+            Console.WriteLine("We have a couple specials going on at the moment:");
+            Console.WriteLine("Buy 2 any loaves of bread, get 1 free!");
+            Console.WriteLine("Buy any pastry for $2 or 3 for $5!");
+
+
         }
     }
 }

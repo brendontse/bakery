19b2138650f15270d07dc3354e8dc07e535a86d9
Brendon <brendonjtse@gmail.comgit config --global user.email brendonjtse@gmail.com>
2019-09-30 00:27:32 -0700

    add bread constructor

--- Bakery.cs
+++ Bakery.cs
@@ -2,5 +2,15 @@ using System;
 
 namespace Bakery.Deals
 {
-    
+    class Bread
+    {
+        public int BreadLoaves { get; set; }
+        public int BreadCost { get; set; }
+        public Bread(int breadLoaves, int breadCost)
+        {
+            BreadLoaves = breadLoaves;
+            BreadCost = breadCost;
+        }
+        
+    }
 }
